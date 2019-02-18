3e811187728cb63c25e9a864e8ab8bfe11ae0c1a
Myntza <tervats@gmail.com>
2019-02-18 22:52:05 +0200

    Teemaa muokkailtu ja viilailtu
    Javascriptiin lisätty myös esto ei ja kyllä valinnan samaa aikaa olemiselle.
    Lisäksi kommentoitu koodia hieman lisää ja viilailtu bootstrapin dokumentaation mukaiseksi

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var btnShow = document.getElementById('kyl').addEventListener('click',showBox);
-var btnHide = document.getElementById('ei').addEventListener('click',hideBox);
+var btnShow = document.getElementById('yes').addEventListener('click',showBox);
+var btnHide = document.getElementById('no').addEventListener('click',hideBox);
+
+
+/* Added true/false to prevent both selected. */
 
 function showBox() {
-
+    no.checked = false;
+    yes.checked = true;
     document.getElementById('needs').style.display="block";
 }
 function hideBox() {
+    no.checked = true;
+    yes.checked = false;
     document.getElementById('needs').style.display="none";
 }
